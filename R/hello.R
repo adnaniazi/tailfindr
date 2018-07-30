45b082d61193deccd5daaecb676824d164549ed2
Adnan Niazi <adnaniazi@gmail.com>
2018-07-30 13:22:25 +0200

    testing

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,4 @@
 hello <- function() {
   print("Hello, world!")
 }
+# bullshit
