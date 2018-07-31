95a0cee155833b3b99bcd3452226532d05ed7c2f
Adnan Niazi <adnaniazi@mac-a-m-n-uib.local>
2018-07-31 09:49:44 +0200

    Added tester main file Updated RbuildIgnore to Ignore raw-data

---
+++ tailfinder_tester.R
@@ -0,0 +1,7 @@
+library(tailfinder)
+library(ggplot2)
+library()
+file_path <-
+    '/Users/adnaniazi/Documents/phd/code/ploy-A-browser/cdna_polyt_fast5s/0.fast5'
+read_data <- extract_read_data(file_path)
+read_data$raw_data
