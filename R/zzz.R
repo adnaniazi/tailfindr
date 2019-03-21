01e963eba2221e1ec9ac0ff1f251808900e34f98
Adnan Niazi <adnaniazi@gmail.com>
2019-03-21 19:45:23 +0100

    Reomved vignette code

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,2 @@
 globalVariables(c("x", "raw_data", "moves"))
 
-# code for making vignette
-.onLoad <- function(libname, pkgname) {
-    vig_list = tools::vignetteEngine(package = 'knitr')
-    vweave <- vig_list[['knitr::knitr']][c('weave')][[1]]
-    vtangle <- vig_list[['knitr::knitr']][c('tangle')][[1]]
-    tools::vignetteEngine(pkgname, weave = vweave, tangle = vtangle,
-                          pattern = "[.]Rmd$", package = pkgname)
-    register_vignette_engines(tailfindr)
-}
