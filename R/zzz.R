0733dde24e2a3b50e3d4a7467ff99053f9044021
Adnan Niazi <adnaniazi@gmail.com>
2019-03-21 19:39:56 +0100

    updated with package name

--- R/zzz.R
+++ R/zzz.R
@@ -7,5 +7,5 @@ globalVariables(c("x", "raw_data", "moves"))
     vtangle <- vig_list[['knitr::knitr']][c('tangle')][[1]]
     tools::vignetteEngine(pkgname, weave = vweave, tangle = vtangle,
                           pattern = "[.]Rmd$", package = pkgname)
-    #register_vignette_engines(pkgname)
+    register_vignette_engines(tailfindr)
 }
